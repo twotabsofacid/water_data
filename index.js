0ff25b6f4c1eda8764d8d85b459c82f1b567bc9d
skiprox <seanmichaelscanlan@gmail.com>
2018-11-28 15:27:51 -0500

    Updating with some ascii art stuff, improving program

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const puppeteer = require('puppeteer');
 const jetpack = require('fs-jetpack');
 const chalk = require('chalk');
 const art = require('./ascii-art');
+let scrapeCount = 0;
 const urls = [
     {
         url: 'https://gardening.stackexchange.com/questions?sort=newest',
@@ -29,6 +30,30 @@ class Water {
     constructor() {
         process.setMaxListeners(Infinity);
         this.addListeners();
+        this.sayHello();
+        for (let [index, urlObj] of urls.entries()) {
+            this.constructPageLinksArray(urlObj.url).then((value) => {
+                this.writeToFiles(urlObj.baseDir, value).then((value) => {
+                    scrapeCount++;
+                    if (scrapeCount == urls.length) {
+                        jetpack.copy('./documents/', '/Volumes/WATER/', {
+                            overwrite: (srcInspectData, destInspectData) => {
+                                return srcInspectData.modifyTime > destInspectData.modifyTime;
+                            }
+                        });
+                        this.sayGoodbye();
+                    }
+                });
+            });
+        }
+    }
+    addListeners() {
+        process.on("unhandledRejection", (reason, p) => {
+            console.error(chalk.red("Unhandled Rejection at: Promise", p, "reason:", reason));
+            process.exit(1);
+        });
+    }
+    sayHello() {
         console.log(chalk.white.bgCyan.bold(`${startString} STARTING ${startString}`));
         console.log(chalk.green(art.flower));
         console.log(chalk.hex("#222").bgHex("#6900DB").bold(`${endString}   this might   ${endString}`));
@@ -41,17 +66,26 @@ class Water {
                 }, 1000)
             }, 1000);
         }, 1000);
-        for (let [index, urlObj] of urls.entries()) {
-            this.constructPageLinksArray(urlObj.url).then((value) => {
-                this.writeToFiles(urlObj.baseDir, value);
-            });
-        }
     }
-    addListeners() {
-        process.on("unhandledRejection", (reason, p) => {
-            console.error(chalk.red("Unhandled Rejection at: Promise", p, "reason:", reason));
-            process.exit(1);
-        });
+    sayGoodbye() {
+        console.log(chalk.white.bgCyan.bold(`\n\n\n\n\n\n\n\n\n\n${startString} ENDING ${startString}`));
+        console.log(chalk.green(art.flower));
+        console.log(chalk.hex("#222").bgHex("#6900DB").bold(`${endString}   thank you   ${endString}`));
+        setTimeout(() => {
+            console.log(chalk.hex("#222").bgHex("#06B1DB").bold(`${endString}  for watering  ${endString}`));
+            setTimeout(() => {
+                console.log(chalk.hex("#222").bgHex("#00DBAE").bold(`${endString} the system ${endString}`));
+                setTimeout(() => {
+                    console.log(chalk.hex("#222").bgHex("#00DB03").bold(`${endString}   please disconnect   ${endString}`));
+                    setTimeout(() => {
+                        console.log(chalk.hex("#222").bgHex("#00DB03").bold(`${endString}   the drive   ${endString}`));
+                        setTimeout(() => {
+                            console.log(chalk.hex("#222").bgHex("#00DB03").bold(`${endString}   safely   ${endString}`));
+                        })
+                    })
+                }, 1000)
+            }, 1000);
+        }, 1000);
     }
     constructPageLinksArray(url) {
         return new Promise((resolve, reject) => {
@@ -125,29 +159,32 @@ class Water {
         });
     }
     writeToFiles(baseDir, value) {
-        for (let obj of value) {
-            let questionCommentsText, answersText;
-            if (obj.questionComments.length) {
-                questionCommentsText = obj.questionComments.reduce(function(accumulator, str) {
-                    return `${accumulator}\n\n${str}`;
-                });
-            } else {
-                questionCommentsText = '';
+        return new Promise((resolve, reject) => {
+            for (let obj of value) {
+                let questionCommentsText, answersText;
+                if (obj.questionComments.length) {
+                    questionCommentsText = obj.questionComments.reduce(function(accumulator, str) {
+                        return `${accumulator}\n\n${str}`;
+                    });
+                } else {
+                    questionCommentsText = '';
+                }
+                if (obj.answers.length) {
+                    answersText = obj.answers.reduce(function (accumulator, str) {
+                        return `${accumulator}\n\n${str}`;
+                    });
+                } else {
+                    answersText = '';
+                }
+                let fileText = `QUESTION:\n${obj.questionText}\n\n\nQUESTION COMMENTS:\n${questionCommentsText}\n\n\nANSWERS:\n${answersText}`;
+                console.log(chalk.white.bgHex("#2FDB8D").bold(`\n\n\n${startString} START OF SCRAPED FILE ${startString}`));
+                console.log(fileText);
+                console.log(chalk.hex("#222").bgHex("#FFFF51").bold(`${endString} END OF SCRAPED FILE ${endString}`));
+                jetpack.dir(`documents/${baseDir}`)
+                    .file(`${obj.questionSlug}.txt`, {content: fileText});
+                resolve(true);
             }
-            if (obj.answers.length) {
-                answersText = obj.answers.reduce(function (accumulator, str) {
-                    return `${accumulator}\n\n${str}`;
-                });
-            } else {
-                answersText = '';
-            }
-            let fileText = `QUESTION:\n${obj.questionText}\n\n\nQUESTION COMMENTS:\n${questionCommentsText}\n\n\nANSWERS:\n${answersText}`;
-            console.log(chalk.white.bgHex("#2FDB8D").bold(`\n\n\n${startString} START OF SCRAPED FILE ${startString}`));
-            console.log(fileText);
-            console.log(chalk.hex("#222").bgHex("#FFFF51").bold(`${endString} END OF SCRAPED FILE ${endString}`));
-            jetpack.dir(`documents/${baseDir}`)
-                .file(`${obj.questionSlug}.txt`, {content: fileText});
-        }
+        });
     }
 }
 
